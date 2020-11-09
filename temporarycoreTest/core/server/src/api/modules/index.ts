ac9a5f6ccf258faba4de77d4c9a10ba9c69d594c
robozavri <nika37912@hotmail.com>
2020-11-09 16:53:01 +0400

    prepared nested fields for gulp

--- temporarycoreTest/core/server/src/api/modules/index.ts
+++ temporarycoreTest/core/server/src/api/modules/index.ts
@@ -1,20 +1,37 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import * as modulesDao from './modules.dao';
 import * as fieldsDao from '../fields/fields.dao';
+import * as configDao from '../configs/config.dao';
 import * as modulesParser  from './modules.parser';
 import * as auth from '../../auth';
-
+import * as _ from 'lodash';
 
 const modulesRouter = Router();
 
 modulesRouter.get('/', modulesParser.parseGetByQuery, getByQuery);
 modulesRouter.post('/', auth.isAdmin, modulesParser.parseCreate, create);
 modulesRouter.post('/generate', auth.isAdmin, modulesParser.parseGenerate, generate);
+modulesRouter.post('/fields', getFields);
 modulesRouter.put('/:id', auth.isAdmin, modulesParser.parseUpdate, update);
 modulesRouter.delete('/:id', auth.isAdmin, destroy);
 modulesRouter.patch('/positions', modulesParser.parseUpdatePositions, updatePositions);
 
 export default modulesRouter;
+
+function findChild(fields: any, parentId: string) {
+  const field = _.find(fields, { parent: parentId });
+  if (!field) {
+    return;
+  }
+  const subChild = findChild(fields, field._id);
+  if (!subChild) {
+    return field;
+  }
+  field[subChild.name] = subChild;
+  // console.log('founded field', field);
+  return field;
+}
+
 
 // =============== GET ===============
 
@@ -28,15 +45,55 @@ async function getByQuery(req: Request, res: Response, next: NextFunction) {
   }
 }
 
+async function getFields(req: Request, res: Response, next: NextFunction) {
+  try {
+    const query = req.query;
+    const modulesData = await modulesDao.getByQuery(query);
+    res.json(modulesData);
+  } catch (e) {
+    next(e);
+  }
+}
+
 // =============== POST ===============
+
+
 
 async function generate(req: Request, res: Response, next: NextFunction) {
   try {
     const payload = req.body;
     const module = await modulesDao.getById(payload._id);
-    const fields = await fieldsDao.getByQuery({ find: { module: payload._id}, limit: 1000});
-    console.log('module', module);
-    res.sendStatus(200);
+    const fieldsData = await fieldsDao.getByQuery({ find: { module: payload._id}, limit: 1000});
+    const configs = await configDao.getOne();
+    const fields = fieldsData.items;
+
+    const nestedFields = [Array];
+
+    fields.map((field) => {
+      if (field.parent === null) {
+        // console.log('passed parent id: ',  field._id);
+        const child =  findChild(fields, field._id);
+        if (!child) {
+          nestedFields.push(field);
+        } else {
+          field[child.name] = child;
+          nestedFields.push(field);
+        }
+      }
+    });
+
+    console.log('nestedFields', nestedFields);
+    // console.log('module', module);
+    // console.log('fields', fieldsData);
+
+    // res.json(nestedFields);
+    res.json(nestedFields);
+    // res.json({
+    //   configs: configs,
+    //   module: module,
+    //   fields: fieldsData,
+    // } );
+    // res.sendStatus(200);
   } catch (e) {
     next(e);
   }
